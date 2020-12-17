9a4e14a76d2ba5c6bf42dde69be425a7f7b496c0
carlschr <cjcarlson136@gmail.com>
2020-12-16 20:49:43 -0600

    Make express server and routes for both htmls

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3300;
+
+//Makes the environment directory equal to the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+//Enables the built-in Express middleware to read JSON
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Root path to return index.html
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html', err => {
+        if (err) throw err;
+    });
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html', err => {
+        if (err) throw err;
+    });
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(__dirname + '/db/db.json', err => {
+        if (err) throw err;
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
+app.listen(PORT, function(err){ 
+    if (err) console.log(err); 
+    console.log("Server listening on PORT", PORT); 
+}); 
